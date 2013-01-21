7ccc4ae53b785cfe6b8322a59835d788466caf8f
Gratien D'haese <gratien.dhaese@gmail.com>
2013-01-21 15:12:41 +0100

    add version numver to file depot close #8

--- make_hpux_depot.sh
+++ make_hpux_depot.sh
@@ -1,6 +1,11 @@
 ################################################################################
 # Version Info : @(#)make_hpux_depot.sh	1.1	04/Apr/2012
 ################################################################################
+if [ -f ./WBEMextras.psf ]; then
+	number="_$(grep number ./WBEMextras.psf | awk '{print $2}')"
+else
+	number=""
+fi
 if [ -d ./WBEMextras.dirdepot.prev ]; then
 	rm -rf ./WBEMextras.dirdepot.prev
 fi
@@ -13,8 +18,8 @@ echo "       * Creating new directory depot WBEMextras.dirdepot"
 /usr/sbin/swpackage -vv -s ./WBEMextras.psf -x layout_version=1.0 \
 	-d ./WBEMextras.dirdepot
 
-echo "       * Creating new file depot WBEMextras.depot"
-/usr/sbin/swpackage -v -d ./WBEMextras.depot -x target_type=tape \
+echo "       * Creating new portable file depot WBEMextras${number}.depot"
+/usr/sbin/swpackage -v -d ./WBEMextras${number}.depot -x target_type=tape \
 	-x media_capacity=4000 -s ./WBEMextras.dirdepot WBEMextras
 
 echo "       * Done."
